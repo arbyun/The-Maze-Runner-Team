39e700cecaf3fb36b6b050a917e4049b61a2bb1b
Daniela Castro <someonealiveh@gmail.com>
2023-03-10 16:40:20 +0000

    feat: class that allows for changing the layer of a game object based on certain conditions

---
+++ Assets/Utilities/LayerRenderingHelper.cs
@@ -0,0 +1,70 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Utilities
+{
+    /// <summary>
+    /// Game manager class for in editor changing the layer of a game object after runtime based on
+    /// certain conditions
+    /// </summary>
+    public class LayerRenderingHelper : MonoBehaviour
+    {
+        [Header("Layer to change")]
+        public LayerMask layerMask;
+
+        [Header("GameObject you want to change layers")]
+        public GameObject gameObjectToChange;
+        
+        [Header("Change layer based on...")]
+        
+        [Header("Time")]
+        public bool useTimer;
+        
+        [ConditionalHide("useTimer", true)] 
+        public float timer;
+        
+        [Header("GameObject (trigger on collision)")]
+        public bool useGameObject;
+        
+        [ConditionalHide("useGameObject", true)]
+        public GameObject gameObj;
+
+        /// <summary>
+        /// Made a mask field in the editor to more easily select the layer the g.o. will
+        /// change to
+        /// </summary>
+        private void OnDrawGizmosSelected()
+        {
+            // created array of strings that will hold the layer names
+            string[] layerNames = new string[32];
+            
+            for (int i = 0; i < 32; i++)
+            {
+                // get the layer name
+                string layerName = LayerMask.LayerToName(i);
+                // if the layer name is empty just give it a default name
+                layerNames[i] = string.IsNullOrEmpty(layerName) ? ("Layer " + i) : layerName;
+            }
+            layerMask = EditorGUILayout.MaskField("Layer", layerMask.value, layerNames);
+        }
+
+        private void Update()
+        {
+            if (useTimer)
+            {
+                if (Time.time > timer)
+                {
+                    gameObjectToChange.layer = layerMask;
+                }
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (useGameObject && other.gameObject == gameObj)
+            {
+                gameObjectToChange.layer = layerMask;
+            }
+        }
+    }
+}
