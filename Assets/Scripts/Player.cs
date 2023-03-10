46beded53439d107f82ca696aabd8e6e779eddb0
Daniela Castro <someonealiveh@gmail.com>
2023-03-10 10:25:36 +0000

    feat(player): (very incomplete) add player script + GetWeapon() function to retrieve equipped weapon object

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using Systems;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool hasWeapon;
+    public float attackDamage;
+
+    /// <summary>
+    /// If there's a weapon equipped, return the weapon. Used in the Damager script to Calculate Damage dealt
+    /// </summary>
+    /// <returns>Return the weapon object equipped</returns>
+    public static GameObject GetWeapon()
+    {
+        GameObject weapon = InventorySystem.EquippedWeapon;
+        return weapon;
+    }
+        
+}
