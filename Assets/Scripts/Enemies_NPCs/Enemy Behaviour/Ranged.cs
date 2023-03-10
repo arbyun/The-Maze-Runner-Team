bc0f0a8a3038e20311d7d1703c4e06006d111158
Daniela Castro <someonealiveh@gmail.com>
2023-03-10 10:19:42 +0000

    feat(enemy): add ranged enemy type + behaviour script

---
+++ Assets/Scripts/Enemies_NPCs/Enemy Behaviour/Ranged.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Enemies_NPCs.Enemy_Behaviour
+{
+    public class Ranged : Enemy
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
