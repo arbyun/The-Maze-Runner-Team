1a07de22b6925e781fa25b8e7501e8619253346b
Daniela Castro <someonealiveh@gmail.com>
2023-03-10 10:29:12 +0000

    feat(InventoryUI): implement OnWeaponClicked method to equip weapon

---
+++ Assets/Scripts/Systems/InventoryUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Systems
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class InventoryUI : MonoBehaviour
+    {
+        private void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnWeaponClicked(GameObject weapon)
+        {
+            InventorySystem inventory = GetComponent<InventorySystem>();
+            inventory.EquipWeapon(weapon);
+        }
+    }
+}
