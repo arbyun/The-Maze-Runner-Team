6eca141af8ba39a793f3777bc21bc4ebd8d41295
Daniela Castro <someonealiveh@gmail.com>
2023-03-10 10:19:22 +0000

    feat(enemy): add jumper enemy type + behaviour script

---
+++ Assets/Scripts/Enemies_NPCs/Enemy Behaviour/Jumper.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Enemies_NPCs.Enemy_Behaviour
+{
+    public class Jumper : Enemy
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
