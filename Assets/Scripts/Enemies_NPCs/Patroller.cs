f682f07adaef8a6063128a6766c0da794cc1f194
Daniela Castro <someonealiveh@gmail.com>
2023-03-10 10:21:36 +0000

    feat: (barebones) implement basic patrolling movement for enemies

---
+++ Assets/Scripts/Enemies_NPCs/Patroller.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Enemies_NPCs
+{
+    public class Patroller : MonoBehaviour
+    {
+        // TODO: make this a full fledged movement mechanic for enemies to patrol an area
+    }
+}
