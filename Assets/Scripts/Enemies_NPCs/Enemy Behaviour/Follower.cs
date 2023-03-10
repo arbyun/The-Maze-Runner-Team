683cce1619dae7f53bd8d0747c7a08c0af181d7a
Daniela Castro <someonealiveh@gmail.com>
2023-03-10 10:18:50 +0000

    feat(enemy): add follower type of enemy + behaviour script

---
+++ Assets/Scripts/Enemies_NPCs/Enemy Behaviour/Follower.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+namespace Enemies_NPCs.Enemy_Behaviour
+{
+    public class Follower : Enemy
+    {
+        [SerializeField] private float speed;
+        private float _visionRange;
+        private float _attackRange;
+        private GameObject _player;
+        private Transform _playerT;
+        private bool _isFollowing;
+        private bool _isAttacking;
+
+        private void Start()
+        {
+            _player = GameObject.FindWithTag("Player");
+            _playerT = _player.transform;
+            _isAttacking = false;
+        }
+
+        /// <summary>
+        /// This function is here for easy animating purposes
+        /// </summary>
+        private static void HasSeenPlayer()
+        {
+            return;
+        }
+
+        private void Update()
+        {
+            // Let's get the distance to the player
+            float distance = Vector3.Distance(transform.position, _playerT.position);
+            
+            // If player is in our vision range, start following them
+            if (distance < _visionRange)
+            {
+                HasSeenPlayer();
+                _isFollowing = true;
+            }
+
+            // If player is in our attack range, attack
+            if (distance < _attackRange)
+            {
+                if (_isAttacking)
+                {
+                    StartCoroutine(DealDamage());
+                    _isAttacking = false;
+                }
+            }
+
+            // Follow the player
+            if (_isFollowing)
+            {
+                transform.LookAt(_playerT.position);
+                transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+            }
+        }
+    }
+}
