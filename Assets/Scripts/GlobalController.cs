1a45e7b07874335568741932870238eb1066ae01
arbyun <someonealiveh@gmail.com>
2023-04-23 21:53:51 +0100

    perf: set up global controller for general game-wide management

---
+++ Assets/Scripts/GlobalController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class GlobalController : MonoBehaviour
+{
+    // Singleton
+    public static GlobalController Instance { get; private set; }
+
+    public GameObject player;
+    public string nextScene;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+}
