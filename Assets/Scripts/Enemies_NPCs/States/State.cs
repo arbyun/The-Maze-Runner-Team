73e5f68c980fb9a3e1126944e219e0fea734cd33
arbyun <someonealiveh@gmail.com>
2023-04-23 21:31:09 +0100

    feat(enemies-npcs): add basic states to enemies for easier management

---
+++ Assets/Scripts/Enemies_NPCs/States/State.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Enemies_NPCs.States
+{
+    public abstract class State
+    {
+        public abstract bool CheckValid(Enemy_Behaviour.Enemy enemyController);
+        public abstract void Execute(Enemy_Behaviour.Enemy  enemyController);
+    }
+}
