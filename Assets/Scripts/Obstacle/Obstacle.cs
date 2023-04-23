45f8b93e07de9ba1e8cdf74ef993e77e8cbf1c73
arbyun <someonealiveh@gmail.com>
2023-04-23 21:50:01 +0100

    feat: set up obstacle script

---
+++ Assets/Scripts/Obstacle/Obstacle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Obstacle
+{
+    public class Obstacle : MonoBehaviour
+    {
+        public void SelfDestroy()
+        {
+            Destroy(gameObject);
+        }
+
+        [SerializeField] private bool pushable; 
+
+        private bool GetPushable() => pushable;
+
+        internal bool SetPushable(bool boolean) => pushable = boolean;
+    }
+}
