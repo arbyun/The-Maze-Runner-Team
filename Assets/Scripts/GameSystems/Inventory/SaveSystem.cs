1344eeea36bf75fe775cacf8f4a1280a83fa74a4
arbyun <someonealiveh@gmail.com>
2023-04-23 21:48:55 +0100

    test(general): play around with the concept of a save system

---
+++ Assets/Scripts/GameSystems/Inventory/SaveSystem.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace GameSystems.Inventory
+{
+    public class SaveSystem : MonoBehaviour
+    {
+        // Place all items in the game by their ID order in this list(ID starts at 0)
+        public List<Item> itemLibrary = new();
+
+        // The string that is use to store the current Inventory Data when SaveInventory is called
+        private string _inventoryString = "";
+
+        public void TransformDataToString()
+        {
+            // For each item the script saves the ID and quantity of it
+            foreach (var item in Inventory.Instance.itemList)
+                _inventoryString = _inventoryString + item.id + ":" +
+                                  Inventory.Instance.quantityList[Inventory.Instance.itemList.IndexOf(item)] + "/";
+        }
+
+        public void SaveInventory()
+        {
+            TransformDataToString();
+            var destination = Application.persistentDataPath + "/save.dat";
+            FileStream file;
+
+            if (File.Exists(destination)) file = File.OpenWrite(destination);
+            else file = File.Create(destination);
+
+            var data = new InventoryData(_inventoryString);
+            var bf = new BinaryFormatter();
+            bf.Serialize(file, data);
+            file.Close();
+        }
+
+        public void LoadInventory()
+        {
+            _inventoryString = "";
+            var destination = Application.persistentDataPath + "/save.dat";
+            FileStream file;
+
+            if (File.Exists(destination))
+            {
+                file = File.OpenRead(destination);
+            }
+            else
+            {
+                Debug.LogError("File not found");
+                return;
+            }
+
+            var bf = new BinaryFormatter();
+            var data = (InventoryData)bf.Deserialize(file);
+            file.Close();
+
+            ReadInventoryData(data.inventoryString);
+
+            // Update UI after Load
+            Inventory.Instance.UpdateInventoryUI();
+        }
+
+        public void ReadInventoryData(string data)
+        {
+            Inventory.Instance.itemList.Clear();
+            Inventory.Instance.quantityList.Clear();
+
+            var splitData = data.Split(char.Parse("/"));
+
+            foreach (var stg in splitData)
+            {
+                var splitID = stg.Split(char.Parse(":"));
+
+                if (splitID.Length >= 2)
+                {
+                    Inventory.Instance.itemList.Add(itemLibrary[int.Parse(splitID[0])]);
+                    Inventory.Instance.quantityList.Add(int.Parse(splitID[1]));
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Utilities/Editor/SaveSystemEditor.cs
@@ -0,0 +1,29 @@
+﻿using GameSystems.Inventory;
+using UnityEditor;
+
+namespace Utilities.Editor
+{
+    [CustomEditor(typeof(SaveSystem))]
+    [CanEditMultipleObjects]
+    public class SaveSystemEditor : UnityEditor.Editor
+    {
+        private SerializedProperty _itemLibrary;
+
+        private void OnEnable()
+        {
+            _itemLibrary = serializedObject.FindProperty("itemLibrary");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.LabelField("Put all the items that exist in this List");
+            EditorGUILayout.LabelField("The items ID must correspond with the place on the List");
+            EditorGUILayout.LabelField("Example: Big Axe ID is 0 so it must be placed in element 0");
+
+
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(_itemLibrary);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
