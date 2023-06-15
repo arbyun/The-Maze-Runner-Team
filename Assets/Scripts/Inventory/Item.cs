517b31ed0b86358bc31f083b971c6fbae8cfef7c
Francisco-Anjos <someonealiveh@gmail.com>
2023-06-15 09:39:11 +0100

    Upload Standalone Build

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -16,53 +16,6 @@ namespace Inventory
         public int quantity = 1;
         public bool equippable;
         public Guid id = new Guid();
-        public ItemType[] types; [Tooltip("Choose only two at max.")]
-
-        internal void Validate()
-        {
-            if (!itemGameObject)
-            {
-                if (!Directory.Exists("Assets/Prefabs"))
-                    AssetDatabase.CreateFolder("Assets", "Prefabs");
-
-                string localPath = "Assets/Prefabs/" + itemName + ".prefab";
-
-                localPath = AssetDatabase.GenerateUniqueAssetPath(localPath);
-
-                bool tempWeapon = false;
-                bool tempConsumable = false;
-
-                GameObject gm = new GameObject($"{itemName}");
-
-                if (types.Length > 1)
-                {
-                    foreach (var type in types)
-                    {
-                        if (type == ItemType.Weapon)
-                        {
-                            tempWeapon = true;
-                        }
-                        else if (type == ItemType.Consumable)
-                        {
-                            tempConsumable = true;
-                        }
-                    }
-                }
-
-                if (tempConsumable)
-                {
-                    gm.AddComponent<Consumable>();
-                }
-
-                if (tempWeapon)
-                {
-                    gm.AddComponent<Weapon>();
-                }
-
-                PrefabUtility.SaveAsPrefabAsset(gm, localPath, out bool idk);
-
-                itemGameObject = gm;
-            }
-        }
+        public ItemType[] types;
     }
 }
